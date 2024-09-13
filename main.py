39ae69fb53f0b78558008c4b404b3753730c1fad
eastmanap <eastmanap354@northwested.org>
2024-09-13 10:31:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
-# Student name
-# Current date
+# Apollos Eastman
+# Sep 13 2024
 # String & Numeric Input 
 
 # Task 1
-# Comment
-# Comment
+print('What is your first name?')
+first_name = input()
+print('Your name is ' + first_name + '.')
+
 
 
 
 
 # Task 2
-# Comment
-# Comment
-
-
-
+print('What is your age?')
+age = int(input())
+print(f'You are {age} years old.')
+print(f'In ten years you will be {age+10}.')
 
 # Task 3
-# Comment
-# Comment
-
-
-
+print('How many people are in your family?')
+fam_num = int(input())
+message = ('There are ' + str(fam_num) + ' people in your family.')
+print (message)
+# print(f'There are {fam_num} people in your family.')
 
 # Task 4
-# Comment
-# Comment
-
-
+print ('what is your height in inches?')
+height = float(input())
+conversion = 2.54
+print ('Your height is ' + f'{height * conversion}' + ' in centimeters.')
 
 
 
